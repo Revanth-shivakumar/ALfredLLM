95860429b537736358a56faa443efbcbad0aa6c2
J <rg@gm.com>
2024-12-06 10:08:05 +0530

    initial route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Home from './pages/Home/Home'
+import { BrowserRouter as Router,Routes,Route, RouterProvider} from 'react-router-dom';
 
+
+const routes =(
+   <Router>
+    <Routes>
+        <Route path='/dashboard' exact element={<Home/>}/>
+        <Route path='/login' exact element={<Login/>}/>
+        <Route path='/signup' exact element={<Signup/>}/>
+    </Routes>
+    </Router>
+
+)
 const App = () => {
   return (
     <div>
-      <Home></Home>
-
+        {routes}
     </div>
   )
 }
