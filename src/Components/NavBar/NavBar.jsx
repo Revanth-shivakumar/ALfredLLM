95024ce08c229fcfcbe91cd45b64f9eb1dab886a
J <rg@gm.com>
2024-12-06 10:29:11 +0530

    Login

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavBar= () => {
+  return (
+    <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow '></div>
+  )
+}
+
+export default NavBar
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import NavBar from '../../Components/NavBar/NavBar'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+     <NavBar/>
+     <div>
+      <div>
+        <form onSubmit={()=>{}}>
+          <h4 className='text-2xl mb-7'>Login</h4>
+          <input type='text' placeholder='Email' className='input-box'/>
+          <button type='submit' className='btn-primary'>
+            Login
+          </button>
+          <p className='text-sm text-center mt-4'>
+          Not Registered yet?{" "}
+          <Link to="/signUp" className="">
+            Create an Account
+          </Link>
+          </p>
+        </form>
+      </div>
+     </div>
+     </>
   )
-}
+};
 
 export default Login
