bd7c42aa58999b62cc8c9c9059e0cbc731446748
J <rg@gm.com>
2024-12-08 18:20:06 +0530

    token autorization

--- backend/utilities.js
+++ backend/utilities.js
@@ -6,5 +6,15 @@ function authenticateToken(req,res,next){
 
     if(!token) return res.sendStatus(401);
 
-    jwt.verify
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
+      if(err) return res.sendStatus(401);
+      req.user=user;
+      next();
+
+
+    });
 }
+
+module.exports(
+    authenticateToken,
+);
