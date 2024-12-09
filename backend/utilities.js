05e0c365492022a0ca94f337cc2d3abda08a7564
J <rg@gm.com>
2024-12-09 18:15:28 +0530

    Backend

--- backend/utilities.js
+++ backend/utilities.js
@@ -1,12 +1,15 @@
 const jwt =require('jsonwebtoken')
+require('dotenv').config()
+
 
 function authenticateToken(req,res,next){
-    const authHeader =req.headers("authorization");
-    const token=authHeader && authHeader.split(" ")[1];
-
-    if(!token) return res.sendStatus(401);
-
+    const authHeader =req.headers["authorization"];
+    const token=authHeader && authHeader.split(" ")[1]?.trim();
+    
+    if(!token)  return res.sendStatus(401);
+            
     jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
+      console.log(err)
       if(err) return res.sendStatus(401);
       req.user=user;
       next();
@@ -15,6 +18,6 @@ function authenticateToken(req,res,next){
     });
 }
 
-module.exports(
+ module.exports={
     authenticateToken,
-);
+};
